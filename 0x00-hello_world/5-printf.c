5863872f5420becfc926af91bb9b03e75d35d34f
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-07-14 00:37:16 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print" with proper grammar, but the outcome 
+ * is a piece of art" followed by a new line.
+ *
+ * Return: Success 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	printf("\n");
+
+	return (0);
+}
