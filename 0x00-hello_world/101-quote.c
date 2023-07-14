8ad6d6ba05c7afb1aae4e18e50f2d6c483477573
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-07-14 01:40:54 -0700

    you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Print exactly "and that piece of art is useful"
- *        - Dora Korpar, 2015-10-19, followed by a newline
+ * Dora Korpar, 2015-10-19, followed by a newline
  *
- * Return 1.
+ * Return: 1.
  */
 int main(void)
 {
 
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	fputs("and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n", stderr);
 
 	return (1);
 }
