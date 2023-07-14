8da4a3251042aa5d09e358ad8a1de342abaa2cdc
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-07-14 01:34:14 -0700

    you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print exactly "and that piece of art is useful"
+ *        - Dora Korpar, 2015-10-19, followed by a newline
+ *
+ * Return 1.
+ */
+int main(void)
+{
+
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+
+	return (1);
+}
