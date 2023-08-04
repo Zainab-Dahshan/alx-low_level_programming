75f31837d35a89312a6472626a6f43f8be00e163
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-08-04 11:14:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * @argc: number of arguments to be passsed
+ * @argv: the string array
+ * Return: 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
