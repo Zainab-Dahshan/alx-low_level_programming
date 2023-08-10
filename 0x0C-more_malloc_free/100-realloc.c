119c0ddfc68cf09580a8b324a48ad0ab52bd913d
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-08-10 14:11:20 +0300

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,37 +19,48 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-	unsigned int i, copy_size;
+	char *p;
 
-	if (new_size == 0)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
+	p = malloc(new_size);
+
+	if (p == NULL)
+		return (NULL);
+
 	if (ptr == NULL)
+		return (p);
+
+	p = _memcpy(p, ptr, (new_size > old_size ? old_size : new_size));
+	free(ptr);
+	return (p);
+}
+
+/**
+ * _memcpy - copies the memory are from
+ * src to dest
+ * @dest: The destination pointer
+ * @src: The source pointer
+ * @n: bytes to use from src
+ *
+ * Return: The pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
+		*(dest + i) = *(src + i);
+		i++;
 	}
 
-	if (new_size == old_size)
-		return (ptr);
-
-	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
-	return (NULL);
-
-	copy_size = old_size < new_size ? old_size : new_size;
-
-	for (i = 0; i < copy_size; i++)
-		((char *)new_ptr)[i] = ((char *)ptr)[i];
-
-	free(ptr);
-
-	return (new_ptr);
+	return (dest);
 }
