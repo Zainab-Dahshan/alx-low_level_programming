cbe53fb047f8d8afe2afe6af4bf6f5d628bced7a
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-08-24 15:04:30 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,36 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stddef.h>
 
 /**
- * print_list - Prints all the elements of a linked list_t list
+ * print_list - function with one argument
+ * @h: pointer to struct
  *
- * @head: Pointer to the head of the linked list
- *
- * Description: This function takes a pointer to the head of
- * a linked list as input, and iterates through the list printing
- * out each element. If an element's `str` field is NULL, it prints
- * `[0] (nil)` instead of the string. The function
- * returns the number of nodes in the list.
- *
- * Return: The number of nodes in the linked list
+ * Description: prints all the elements of a list
+ * Return: number of nodes
  */
-size_t print_list(const list_t *head)
+size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	int count = 0;
 
-	while (head != NULL)
+	while (h != NULL)
 	{
-		if (head->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s\n", head->len, head->str);
-		}
+		printf("%p\n", (void *)h->next);
+		h = h->next;
 		count++;
-		head = head->next;
 	}
 	return (count);
 }
