389cc670a993f0a8fe9e61558e101932e62c35b4
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-08-28 03:19:43 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,10 +5,10 @@
 
 /**
  * print_listint - main function to print
- *  all the elements of a listint_t list.
- * @h: A pointer to the head of the linked list.
+ *  all elements of a listint_t list.
+ * @h: pointer to the head of the linked list.
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes in the list.
  */
 size_t print_listint(const listint_t *h)
 {
@@ -22,29 +22,4 @@ size_t print_listint(const listint_t *h)
 	}
 
 	return (count);
-}
-
-/**
- * main - main entry point
- *
- * Return: alwyas 0.
- */
-int main(void)
-{
-	size_t count;
-	listint_t *head = malloc(sizeof(listint_t));
-
-	head->n = 1;
-	head->next = malloc(sizeof(listint_t));
-	head->next->n = 2;
-	head->next->next = NULL;
-
-     	count = print_listint(head);
-
-	printf("List contains %lu nodes.\n", (unsigned long)count);
-
-	free(head->next);
-	free(head);
-
-	return (0);
 }
