ba8c77eb0116294241c851f28bc69ad1cfcab906
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-08-29 18:09:04 -0700

    b

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - main function to add a new node
+ * at the beginning of a listint_t list.
+ * @head: pointer to pointer to the head node of the list.
+ * @n: The integer to be stored in the new node.
+ *
+ * Return: If memory allocation fails or head is NULL, NULL.
+ *         Otherwise, the address of the new element.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - main function to add a new node
+ * at the end of a listint_t list.
+ * @head: pointer to pointer to the head node of the list.
+ * @n: The integer to be stored in the new node.
+ *
+ * Return: If memory allocation fails or head is NULL, NULL.
+ * Otherwise, the address of the new element.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *last;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	temp = *head;
+
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	temp->next = new_node;
+
+	return (new_node);
+}
