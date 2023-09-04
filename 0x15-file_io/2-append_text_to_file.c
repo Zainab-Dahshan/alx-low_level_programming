f28644b6afa81a6c6ace2ebed6bf05104d7b4903
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-09-04 05:35:00 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,40 +1,39 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <string.h>
 
 /**
- * append_text_to_file - main function that append text.
- * @filename: a file name.
- * @text_content: a NULL terminated str added to a file.
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file.
+ * @text_content: NULL-terminated str to add at the end of a file.
  * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND);
-	int length = strlen(text_content);
-	ssize_t bytes_written = write(fd, text_content, length);
+	int fd, bytes_written;
+	size_t text_len;
 
 	if (filename == NULL)
-	{
-	fprintf(stderr, "[ERROR] Invalid input. filename cannot be NULL.");
 	return (-1);
-	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
-	{
-	fprintf(stderr, "[ERROR] Failed to open file: %s", filename);
 	return (-1);
-	}
-	if (bytes_written == -1)
+
+	if (text_content != NULL)
 	{
-	fprintf(stderr, "[ERROR] Failed to write to file: %s", filename);
+		text_len = strlen(text_content);
+		bytes_written = write(fd, text_content, text_len);
+
+	if (bytes_written != (int)text_len)
+	{
 	close(fd);
 	return (-1);
 	}
+	}
+
 	close(fd);
 	return (1);
 }
