1e7751d2856a7fba7f88f46d99304061204160b2
Zainab-Dahshan <zainabeldahshan@gmail.com>
2023-09-04 05:18:14 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,9 +4,9 @@
 #include <string.h>
 
 /**
- * append_text_to_file - main function that append text at the end of a file.
+ * append_text_to_file - main function that append text.
  * @filename: a file name.
- * @text_content: a NULL terminated str added at the end of the file.
+ * @text_content: a NULL terminated str added to a file.
  * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
